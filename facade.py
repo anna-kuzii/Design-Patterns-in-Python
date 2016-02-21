429a4f95f2c4e69905b09e8ea1aed357e8a5af3e
Anna <anna_kuzij@ukr.net>
2016-02-21 19:00:01 +0200

    Update facade.py

--- facade.py
+++ facade.py
@@ -1,13 +1,15 @@
 # Complex parts
 class CPU:
-    def freeze(self): pass
+    def __init__(self): pass
     def jump(self, position): pass
     def execute(self): pass
 
 class Memory:
+    def __init__(self): pass
     def load(self, position, data): pass
 
 class HardDrive:
+    def __init__(self): pass
     def read(self, lba, size): pass
 
 # Facade
@@ -17,13 +19,12 @@ class Computer:
         self.memory = Memory()
         self.hard_drive = HardDrive()
 
-    def start_computer(self):
-        self.cpu.freeze()
-        self.memory.load(0, self.hard_drive.read(0, 1024))
+    def start(self):
+        self.mem.load(0, self.hard_drive.read(0, 1024))
         self.cpu.jump(10)
         self.cpu.execute()
 
 # Client
 if __name__ == '__main__':
     facade = Computer()
-    facade.start_computer()
+    facade.start()
